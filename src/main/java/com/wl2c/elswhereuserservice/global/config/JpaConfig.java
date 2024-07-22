949c575e92df42321c0018a8b35a36b7ca61507d
kjungw1025 <kjungw1025@naver.com>
2024-07-22 16:36:15 +0900

    conf: 데이터베이스 관련 의존성 추가 및 BaseEntity 추가

---
+++ src/main/java/com/wl2c/elswhereuserservice/global/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.wl2c.elswhereuserservice.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
