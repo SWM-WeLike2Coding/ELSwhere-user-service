199228853c3da3f7599676be2a6fe8982db200ae
kjungw1025 <kjungw1025@naver.com>
2024-07-23 11:13:29 +0900

    feat: User 엔티티 추가

---
+++ src/main/java/com/wl2c/elswhereuserservice/domain/user/model/entity/User.java
@@ -0,0 +1,63 @@
+package com.wl2c.elswhereuserservice.domain.user.model.entity;
+
+import com.wl2c.elswhereuserservice.domain.user.model.SocialType;
+import com.wl2c.elswhereuserservice.domain.user.model.UserStatus;
+import com.wl2c.elswhereuserservice.global.auth.role.UserRole;
+import com.wl2c.elswhereuserservice.global.base.BaseEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import static jakarta.persistence.EnumType.STRING;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "user_id")
+    private Long id;
+
+    @NotNull
+    private String socialId;
+
+    @NotNull
+    private SocialType socialType;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    @Column(length = 20)
+    private String name;
+
+    @NotNull
+    @Column(length = 20)
+    private String nickname;
+
+    @Enumerated(STRING)
+    private UserStatus userStatus;
+
+    @Enumerated(STRING)
+    private UserRole userRole;
+
+    @Builder
+    private User (@NonNull String socialId,
+                  @NonNull SocialType socialType,
+                  @NonNull String email,
+                  @NonNull String name,
+                  @NonNull String nickname,
+                  UserStatus userStatus,
+                  UserRole userRole) {
+        this.socialId = socialId;
+        this.socialType = socialType;
+        this.email = email;
+        this.name = name;
+        this.nickname = nickname;
+        this.userStatus = userStatus;
+        this.userRole = userRole;
+    }
+}
