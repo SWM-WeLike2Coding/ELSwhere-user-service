4e2dd7e4b825e37c29088c67affb2486b2306fdb
kjungw1025 <kjungw1025@naver.com>
2024-07-26 10:09:02 +0900

    feat: 토큰 재발급 기능 추가

--- src/main/java/com/wl2c/elswhereuserservice/domain/user/controller/UserController.java
+++ src/main/java/com/wl2c/elswhereuserservice/domain/user/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.wl2c.elswhereuserservice.domain.user.controller;
 
 import com.wl2c.elswhereuserservice.domain.user.model.dto.request.RequestNickNameChangeDto;
+import com.wl2c.elswhereuserservice.domain.user.model.dto.request.RequestRefreshTokenDto;
+import com.wl2c.elswhereuserservice.domain.user.model.dto.response.ResponseRefreshTokenDto;
 import com.wl2c.elswhereuserservice.domain.user.model.dto.response.ResponseUserInfoDto;
 import com.wl2c.elswhereuserservice.domain.user.service.UserInfoService;
 import com.wl2c.elswhereuserservice.domain.user.service.UserService;
@@ -40,5 +42,17 @@ public class UserController {
     public void changeNickName(HttpServletRequest request,
                                @Valid @RequestBody RequestNickNameChangeDto dto) {
         userService.changeNickname(parseLong(request.getHeader("requestId")), dto);
+    }
+
+    /**
+     * 토큰 재발급
+     *
+     * @param dto 요청 body
+     * @return 재발급된 로그인 인증 정보
+     */
+    @PostMapping("/reissue")
+    public ResponseRefreshTokenDto refreshToken(HttpServletRequest request,
+                                                @Valid @RequestBody RequestRefreshTokenDto dto) {
+        return userService.refreshToken(request, dto.getRefreshToken());
     }
 }
