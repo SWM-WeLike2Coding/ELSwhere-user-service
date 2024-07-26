c885e1c8de2d7e9ed769f5437a885737fb38ce75
kjungw1025 <kjungw1025@naver.com>
2024-07-26 19:34:01 +0900

    feat: 사용자의 보유 상품 금액 수정에 대한 리포지토리 인터페이스 추가

--- src/main/java/com/wl2c/elswhereuserservice/domain/user/repository/UserHoldingRepository.java
+++ src/main/java/com/wl2c/elswhereuserservice/domain/user/repository/UserHoldingRepository.java
@@ -2,8 +2,18 @@ package com.wl2c.elswhereuserservice.domain.user.repository;
 
 import com.wl2c.elswhereuserservice.domain.user.model.entity.Holding;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.math.BigDecimal;
 
 public interface UserHoldingRepository extends JpaRepository<Holding, Long> {
 
-
+    @Modifying(clearAutomatically = true)
+    @Query("update Holding h set h.price = :price " +
+            "where h.id = :id and h.user.id = :userId ")
+    void updateHolding(@Param("userId") Long userId,
+                       @Param("id") Long id,
+                       @Param("price") BigDecimal price);
 }
