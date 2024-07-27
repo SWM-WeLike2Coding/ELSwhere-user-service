46b3793fe470e1f981a4d6361caf8d278d49645c
kjungw1025 <kjungw1025@naver.com>
2024-07-27 22:59:48 +0900

    fix: 보유 상품의 수정 삭제 기능에 대한 요청 양식 수정

--- src/main/java/com/wl2c/elswhereuserservice/domain/user/controller/UserHoldingController.java
+++ src/main/java/com/wl2c/elswhereuserservice/domain/user/controller/UserHoldingController.java
@@ -1,15 +1,15 @@
 package com.wl2c.elswhereuserservice.domain.user.controller;
 
 import com.wl2c.elswhereuserservice.domain.user.model.dto.request.RequestCreateHoldingDto;
-import com.wl2c.elswhereuserservice.domain.user.model.dto.request.RequestUpdateHoldingDto;
 import com.wl2c.elswhereuserservice.domain.user.service.UserHoldingService;
 import com.wl2c.elswhereuserservice.global.model.dto.ResponseIdDto;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
 
 import static java.lang.Long.parseLong;
 
@@ -23,6 +23,12 @@ public class UserHoldingController {
 
     /**
      * 보유 상품 등록
+     * <p>
+     *     <br/>
+     *     등록 시, 사용자의 보유 상품 id를 반환하게 됩니다.
+     *     해당 보유 상품 id로 조회, 수정 및 삭제가 가능합니다.
+     * </p>
+     *
      * @param dto 상품 등록을 위한 정보(상품 id, 금액)
      * @return 보유 상품 id
      */
@@ -34,20 +40,25 @@ public class UserHoldingController {
 
     /**
      * 특정 보유 상품 금액 수정
-     * @param dto 보유 상품 수정을 위한 정보(상품 id, 금액)
+     *
+     * @param id 수정할 보유 상품 id
+     * @param price 수정할 금액
      */
-    @PatchMapping
+    @PatchMapping("/{id}")
     public void update(HttpServletRequest request,
-                       @Valid @RequestBody RequestUpdateHoldingDto dto) {
-        userHoldingService.update(parseLong(request.getHeader("requestId")), dto);
+                       @PathVariable Long id,
+                       @RequestParam("price") BigDecimal price) {
+        userHoldingService.update(parseLong(request.getHeader("requestId")), id, price);
     }
 
     /**
      * 특정 보유 상품 삭제
+     *
+     * @param id 삭제할 보유 상품 id
      */
-    @DeleteMapping
-    public void update(HttpServletRequest request,
-                       @RequestParam Long holdingId) {
-        userHoldingService.delete(parseLong(request.getHeader("requestId")), holdingId);
+    @DeleteMapping("/{id}")
+    public void delete(HttpServletRequest request,
+                       @PathVariable Long id) {
+        userHoldingService.delete(parseLong(request.getHeader("requestId")), id);
     }
 }
--- src/main/java/com/wl2c/elswhereuserservice/domain/user/service/UserHoldingService.java
+++ src/main/java/com/wl2c/elswhereuserservice/domain/user/service/UserHoldingService.java
@@ -3,7 +3,6 @@ package com.wl2c.elswhereuserservice.domain.user.service;
 import com.wl2c.elswhereuserservice.domain.user.exception.HoldingNotFoundException;
 import com.wl2c.elswhereuserservice.domain.user.exception.UserNotFoundException;
 import com.wl2c.elswhereuserservice.domain.user.model.dto.request.RequestCreateHoldingDto;
-import com.wl2c.elswhereuserservice.domain.user.model.dto.request.RequestUpdateHoldingDto;
 import com.wl2c.elswhereuserservice.domain.user.model.entity.Holding;
 import com.wl2c.elswhereuserservice.domain.user.model.entity.User;
 import com.wl2c.elswhereuserservice.domain.user.repository.UserHoldingRepository;
@@ -13,6 +12,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
@@ -41,13 +42,13 @@ public class UserHoldingService {
     }
 
     @Transactional
-    public void update(Long userId, RequestUpdateHoldingDto requestUpdateHoldingDto) {
-        Holding holding = userHoldingRepository.findById(requestUpdateHoldingDto.getHoldingId()).orElseThrow(HoldingNotFoundException::new);
+    public void update(Long userId, Long holdingId, BigDecimal price) {
+        Holding holding = userHoldingRepository.findById(holdingId).orElseThrow(HoldingNotFoundException::new);
 
         if (holding.getUser().getId().equals(userId)) {
             userHoldingRepository.updateHolding(userId,
-                    requestUpdateHoldingDto.getHoldingId(),
-                    requestUpdateHoldingDto.getPrice());
+                    holdingId,
+                    price);
         } else {
             throw new UserNotFoundException();
         }
@@ -63,4 +64,12 @@ public class UserHoldingService {
             throw new UserNotFoundException();
         }
     }
+
+    // TODO: 전체 보유 상품 금액 합계(현재 보유중인 상품과 상환 완료한 상품 금액 구분해서 보여주는 리스트)
+
+    // TODO: 특정 보유 상품에 대한 평가 금액 반영 및 낙인 도달 여부 확인
+
+    // TODO: 현재 보유중인 상품 리스트
+
+    // TODO: 상환 완료한 상품 금액 구분해서 보여주는 리스트
 }
