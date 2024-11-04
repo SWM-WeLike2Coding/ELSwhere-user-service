50de75655dfd557547176446192c375d6793d2c2
kjungw1025 <kjungw1025@naver.com>
2024-11-04 18:45:44 +0900

    feat: 상품 서비스에서 청약 중인 상품들을 받아오기 위해 상품 서비스 feignclient 인터페이스에 메소드 추가

--- src/main/java/com/wl2c/elswhereuserservice/client/product/api/ProductServiceClient.java
+++ src/main/java/com/wl2c/elswhereuserservice/client/product/api/ProductServiceClient.java
@@ -2,10 +2,13 @@ package com.wl2c.elswhereuserservice.client.product.api;
 
 import com.wl2c.elswhereuserservice.client.product.dto.request.RequestProductIdListDto;
 import com.wl2c.elswhereuserservice.client.product.dto.response.ResponseSingleProductDto;
-import com.wl2c.elswhereuserservice.client.product.dto.response.ResponseSummarizedProductDto;
+import com.wl2c.elswhereuserservice.client.product.dto.list.SummarizedProductDto;
 import com.wl2c.elswhereuserservice.client.product.dto.response.ResponseSummarizedProductForHoldingDto;
+import com.wl2c.elswhereuserservice.global.model.dto.ResponsePage;
 import jakarta.validation.Valid;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,8 +19,11 @@ public interface ProductServiceClient {
     @GetMapping("/v1/product/{productId}")
     ResponseSingleProductDto getProduct(@PathVariable Long productId);
 
+    @GetMapping("/v1/product/on-sale")
+    ResponsePage<SummarizedProductDto> listByOnSale(@RequestParam(name = "type") String type, @ParameterObject Pageable pageable);
+
     @PostMapping("/v1/product/list")
-    List<ResponseSummarizedProductDto> listByProductIds(@Valid @RequestBody RequestProductIdListDto requestProductIdListDto);
+    List<SummarizedProductDto> listByProductIds(@Valid @RequestBody RequestProductIdListDto requestProductIdListDto);
 
     @PostMapping("/v1/product/holding/list")
     List<ResponseSummarizedProductForHoldingDto> holdingListByProductIds(@Valid @RequestBody RequestProductIdListDto requestProductIdListDto);
